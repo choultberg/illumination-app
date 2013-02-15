37952361761069dc1ccb863dfc0c5c2db82050ef
Jeff Crouse <jefftimesten@gmail.com>
2013-02-15 17:30:25 -0600

    form elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+
+$(document).ready(function(){
+
+
+
+
+	$('a.scrollTo').on({
+		click: function() {
+		    $('html, body').animate({
+		        scrollTop: $( $(this).attr('href') ).offset().top
+		    }, 500);
+    		return false;
+		}
+	});
+});
